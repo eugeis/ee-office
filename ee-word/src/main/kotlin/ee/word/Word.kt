0dfe257a49c47e7e3c5e1af619188d7962ce0cb5
Eugen Eisler <eoeisler@gmail.com>
2017-07-05 13:32:43 +0200

    Add MS Word support

---
+++ ee-word/src/main/kotlin/ee/word/Word.kt
@@ -0,0 +1,35 @@
+package ee.word
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument
+import org.slf4j.LoggerFactory
+import java.io.FileInputStream
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.text.SimpleDateFormat
+
+private val log = LoggerFactory.getLogger(Word::class.java)
+private val dateParser = SimpleDateFormat("DD.MM.YYYY")
+
+
+class Word {
+
+    companion object {
+        @JvmStatic
+        fun open(fileName: String): XWPFDocument {
+            return XWPFDocument(FileInputStream(Paths.get(fileName).toFile()))
+        }
+
+        @JvmStatic
+        fun write(document: XWPFDocument, fileName: String) {
+            val outputPath = Paths.get(fileName)
+            try {
+                Files.newOutputStream(outputPath).use {
+                    document.write(it)
+                }
+            } catch (e: IOException) {
+                throw e
+            }
+        }
+    }
+}
